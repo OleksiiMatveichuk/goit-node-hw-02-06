ed42db6725351f800fbe64ca67f746535f601c6f
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-07-26 13:53:40 +0300

    add logick

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,58 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
+const updateContacts = async (contacts) =>
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const data = await listContacts();
+  const result = data.filter((el) => el.id === contactId);
+  return result || null;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const data = await listContacts();
+  const index = data.findIndex((el) => el.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = data.splise(index, 1);
+  await updateContacts(data);
+  return result;
+  // нащо 2 змінні. різалт і дата. чи можна одну?
+  // data.splise(index, 1);
+  // await updateContacts(data);
+  // return data;
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  const data = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...body,
+  };
+  data.push(newContact);
+  await updateContacts(data);
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const data = await listContacts();
+  const index = data.findIndex((el) => el.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  data[index] = { contactId, ...body };
+  await updateContacts(data);
+  return data[index];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +60,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,40 @@
-const express = require('express')
+const express = require("express");
+const contacts = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id);
+    if (!result) {
+      res.status(404).message({ message: "Not found" });
+    }
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+module.exports = router;
